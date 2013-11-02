ccefb0ea1de9007694f753ca5144736fbff595f5
AlekSash <virara@yandex.ru>
2013-11-02 22:51:34 +0700

    Jwnl library added.

--- java/KursGraphApp.java
+++ java/KursGraphApp.java
@@ -1,9 +1,22 @@
-
-
 public class KursGraphApp
 {
     public static void main(String[] args)
     {
-	System.out.println("Hot greeting to Alexandra!");
+        // construct the URL to the Wordnet dictionary directory
+        String wnhome = System.getenv("WNHOME");
+        String path = wnhome + File.separator + "dict";
+        URL url = new URL("file", null, path);
+
+        // construct the dictionary object and open it
+        IDictionary dict = new Dictionary(url);
+        dict.open();
+
+        // look up first sense of the word "dog"
+        IIndexWord idxWord = dict.getIndexWord("dog", POS.NOUN);
+        IWordID wordID = idxWord.getWordIDs().get(0);
+        IWord word = dict.getWord(wordID);
+        System.out.println("Id = " + wordID);
+        System.out.println("Lemma = " + word.getLemma());
+        System.out.println("Gloss = " + word.getSynset().getGloss());
     }
 }
