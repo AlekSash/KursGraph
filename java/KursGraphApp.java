2fe51f0d560c7f56a958069ca48f4bc1fdd2d8cb
AlekSash <virara@yandex.ru>
2014-02-17 08:22:09 +0700

    Iterators

--- java/KursGraphApp.java
+++ java/KursGraphApp.java
@@ -1,14 +1,15 @@
 
 import java.io.PrintWriter;
 import java.lang.Integer;
+import java.lang.Iterable;
 import java.lang.Object;
 import java.lang.String;
 import java.util.*;
 import java.io.*;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
+import java.util.SortedSet;
 import java.util.Vector;
 
 import net.didion.jwnl.JWNL;
@@ -22,44 +23,77 @@ import net.didion.jwnl.dictionary.Dictionary;
 class Node
 {
     public Object node;
-    public Vector adjacentNodes = new Vector();
+
+    private Vector adjacentNodes = new Vector();
 
     public Node(Object elem)
     {
       node = elem;
     };
 
+    public Iterator CreateIterator()
+    {
+      return adjacentNodes.iterator();
+    };
+
     public void addArc(Object elem)
     {
         adjacentNodes.add(elem);
-    }
+    };
 
     public void delArc(Object elem)
     {
         adjacentNodes.removeElement(elem);
-    }
+    };
 }
+
 
 class Graph
 {
     private Vector<Node> nodes = new Vector<Node>();
 
     // This method provides access to the Node object which is supposed to be a part of internal structure, it is not a good idea!
-    public Object GetElem(int index)
+    private Object GetElem(int index)
     {
         return nodes.get(index);
-    }
+    };
+
+    public Iterator CreateIterator()
+    {
+        return nodes.iterator();
+    };
+
+    public int GetSize()
+    {
+      return nodes.size();
+    };
 
     public void AddNode(Object v)
     {
-	nodes.add(new Node(v));
-    }
+	    nodes.add(new Node(v));
+    };
 
     public void DeleteNode(int index)
     {
-	if (index >= 0 && index < nodes.size())
-	    nodes.removeElementAt(index);
-    }
+	    if (index >= 0 && index < nodes.size())
+        {   Node v = nodes.get(index);
+	        nodes.removeElementAt(index);
+            Iterator it1 =this.CreateIterator();
+            while (it1.hasNext())
+            {
+                Node p = it1.next();
+                Iterator it2 = p.CreateIterator();
+                while(it2.hasNext())
+                {
+                   Object a = it2.next();
+                    if(a==v.node)
+                        p.delArc(a);
+                }
+            }
+            }
+
+        };
+
 
     public void AddArc(Object source, Object target)
     {
@@ -73,17 +107,20 @@ class Graph
 	sourceNode.addArc(target);
 	if (nodes.indexOf(target) == -1)
 	    nodes.add(new Node(target));
-    }
+    };
 
-    public void DeleteArc(int source, int target)
+    public void DeleteArc(Object source, Object target)
     {
-	if (source < 0 || source >= nodes.size() ||
-	    target < 0 || target >= nodes.size())
-	    return;
-        Node a = nodes.get(source);
-        a.delArc(nodes.get(target).node);
-    }
+        int sourceIndex = nodes.indexOf(source);
+        int targetIndex = nodes.indexOf(target);
+        if (sourceIndex < 0 || sourceIndex >= nodes.size() ||
+            targetIndex < 0 || targetIndex>= nodes.size())
+            return;
+        Node sourceNode = nodes.get(sourceIndex);
+        sourceNode.delArc(target);
+    };
 }
+
 
 public class KursGraphApp
 {
@@ -91,30 +128,18 @@ public class KursGraphApp
     {
         JWNL.initialize(new FileInputStream("file_properties.xml"));
         Dictionary dict = Dictionary.getInstance();
-        //итератор проходится по всем существительным
         Iterator it = dict.getIndexWordIterator(POS.NOUN);
-        Iterator it1 = dict.getIndexWordIterator(POS.NOUN);
-        //файл для записи слов
         PrintWriter writer = new PrintWriter("output.txt", "UTF-8");
-        PrintWriter tester = new PrintWriter("test.txt");
-        //Мар для записи пары ключ, слово
-        Map<Integer, String> words= new TreeMap<Integer, String>();
-        Set kv = words.entrySet();
-
-
-
-
-
+        PrintWriter tester = new PrintWriter("test.txt",  "UTF-8");
         Graph tes = new Graph();
-        int count = 0;
+        Iterator it1 = tes.CreateIterator();
         while(it.hasNext())
         {   //текущее слово
             IndexWord current = (IndexWord)it.next();
             //получение леммы
             String lemma = current.getLemma();
-            count++;
-            tes.AddNode(/*count,*/lemma);
-            tester.println(tes.GetElem(count));
+            tes.AddNode(lemma);
+            tester.println(it1.next());
             //печать леммы
             writer.print(lemma);
             writer.println();
---
+++ run.bat
@@ -0,0 +1,2 @@
+java -cp ./lib/commons-logging.jar;./lib/junit-4.1.jar;./lib/jwnl.jar;jar/kursgraph.jar KursGraphApp
+
