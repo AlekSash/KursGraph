d0b44d7777740505d4c1cfb46ddf70dbd6889553
Michael Pozhidaev <msp@altlinux.ru>
2014-04-29 11:25:49 +0700

    Needless import entries removed

--- java/graph.java
+++ java/graph.java
@@ -1,28 +1,5 @@
 
-import java.io.PrintWriter;
-import java.lang.*;
-import java.lang.Integer;
-import java.lang.Iterable;
-import java.lang.NullPointerException;
-import java.lang.Object;
-import java.lang.String;
-import java.lang.System;
 import java.util.*;
-import java.io.*;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.SortedSet;
-import java.util.Vector;
-
-import net.didion.jwnl.JWNL;
-import net.didion.jwnl.data.IndexWord;
-import net.didion.jwnl.data.Word;
-import net.didion.jwnl.data.POS;
-import net.didion.jwnl.data.Synset;
-import net.didion.jwnl.data.Pointer;
-import net.didion.jwnl.dictionary.Dictionary;
-import sun.security.provider.certpath.Vertex;
 
 /*класс графа*/
 class Graph
